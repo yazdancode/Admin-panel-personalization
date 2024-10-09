9c2b3021267bc9b8dec254e926f1433e1ca5e50d
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-10-08 22:36:27 -0700

    0.06

--- Concert/settings.py
+++ Concert/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "adminlte3",
+    "adminlte3_theme",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
